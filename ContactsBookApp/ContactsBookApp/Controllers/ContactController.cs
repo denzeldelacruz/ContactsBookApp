75bd8aa346335f5438e84b9b500b893935b2c097
Denzel dela Cruz <denzeldelacruz17@gmail.com>
2016-03-28 15:01:10 +0800

    03/28/2016 : First commit of ContactsBookApp

---
+++ ContactsBookApp/ContactsBookApp/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContactsBookApp.Models;
+
+namespace ContactsBookApp.Controllers
+{
+    public class ContactController : Controller
+    {
+        private ContactDbContext db = new ContactDbContext();
+
+        // GET: Contact
+        public ActionResult Index()
+        {
+            ViewBag.Contacts = db.Contacts.ToList();
+            return View();
+        }
+    }
+}
---
+++ ContactsBookApp/ContactsBookApp/Models/ContactModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactsBookApp.Models
+{
+    [Table("tbl_contact")]
+    public class Contact
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string contact_no { get; set; }
+        public string email_addr { get; set; }
+        public string addr { get; set; }
+        public int? vip { get; set; }
+    }
+
+    public class ContactDbContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
